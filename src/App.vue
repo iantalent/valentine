aa95221d0393d60e6006e0c6894149998cfff453
sea <iantalent95@gmail.com>
2023-02-14 14:21:34 +0200

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div>
+		<EnvelopeContainer>
+			<h1>Дорогая особа!</h1>
+			
+			<p>В этот день любви я хочу сказать <br>как много ты значишь для меня.<br> Хочу тебя пригласить на свидание.</p>
+		</EnvelopeContainer>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	import EnvelopeContainer from "@/components/EnvelopeContainer";
+	
+	export default {
+		name: 'App',
+		components: {EnvelopeContainer}
+	};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+	@import "assets/variables";
+	
+	html, body {
+		width: 100%;
+		height: 100%;
+	}
+	
+	#app {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+	}
+	
+	body {
+		background: $bgcolor;
+		padding: 0;
+		margin: 0;
+		position: relative;
+		font-family: 'Marck Script', sans-serif;
+	}
+	
+	body h1 {
+		font-size: $h1-size;
+		line-height: $h1-size + 2px;
+		margin: 5px 0 35px;
+	}
+	
+	body p {
+		font-size: $text-size;
+		line-height: $text-size + 2px;
+	}
 </style>
